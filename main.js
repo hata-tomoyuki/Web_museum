ee7fae8815e8bc449d6793549af0cff2e97bd117
hata-tomoyuki <h.tomo0602@gmail.com>
2022-04-05 19:11:23 +0900

    初期セットアップ

---
+++ main.js
@@ -0,0 +1,195 @@
+import * as THREE from "./build/three.module.js";
+import { PointerLockControls } from "./controls/PointerLockControls.js";
+
+//前進か後進か変数宣言
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+
+//移動速度と移動方向の定義
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
+const color = new THREE.Color();
+
+/**
+ *  シーン
+ **/
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+scene.fog = new THREE.Fog(0xffffff, 0, 750);
+
+/**
+ *  カメラ
+ **/
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 1, 2);
+
+/**
+ *  レンダラー
+ **/
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/**
+ *  ライト
+ **/
+const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+light.position.set(0.5, 1, 0.75);
+scene.add(light);
+
+
+//FPS視点設定
+const controls = new PointerLockControls(camera, renderer.domElement);
+
+if (typeof window.ontouchstart === "undefined") {
+  window.addEventListener("click", () => {
+    controls.lock();
+  })
+} else {
+  window.addEventListener("touchstart", onTouchStart);
+}
+
+function onTouchStart(event) {
+
+  // タッチ開始位置
+  let touched_pos= new THREE.Vector2();
+
+  touched_pos.x = event.changedTouches[0].pageX - (window.innerWidth / 2);
+  touched_pos.y = - (event.changedTouches[0].pageY - (window.innerHeight / 2));
+  
+}
+
+/**
+ * オブジェクト生成
+ **/
+const planeGeometry = new THREE.PlaneGeometry(400, 400, 100, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: "orange",
+  wireframe: true,
+});
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
+
+const boxGeometry = new THREE.BoxGeometry(20, 20, 20);
+let position = boxGeometry.attributes.position;
+const colorsBox = [];
+for (let i = 0, l = position.count; i < l; i++) {
+  color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+  colorsBox.push(color.r, color.g, color.b);
+}
+boxGeometry.setAttribute(
+  "color",
+  new THREE.Float32BufferAttribute(colorsBox, 3)
+);
+for (let i = 0; i < 200; i++) {
+  const boxMaterial = new THREE.MeshPhongMaterial({
+    specular: 0xffffff,
+    flatShading: true,
+    vertexColors: true,
+  });
+  boxMaterial.color.setHSL(
+    Math.random() * 0.2 + 0.5,
+    0.75,
+    Math.random() * 0.25 + 0.75
+  );
+  const box = new THREE.Mesh(boxGeometry, boxMaterial);
+  box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
+  box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
+  box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
+  scene.add(box);
+}
+
+//キーボード操作
+const onKeyDown = (e) => {
+  switch(e.code) {
+    case "KeyW":
+      moveForward = true;
+      break;
+    case "KeyA":
+      moveLeft = true;
+      break;
+    case "KeyS":
+      moveBackward = true;
+      break;
+    case "KeyD":
+      moveRight = true;
+      break;
+  }
+};
+
+const onKeyUp = (e) => {
+  switch(e.code) {
+    case "KeyW":
+      moveForward = false;
+      break;
+    case "KeyA":
+      moveLeft = false;
+      break;
+    case "KeyS":
+      moveBackward = false;
+      break;
+    case "KeyD":
+      moveRight = false;
+      break;
+  }
+};
+
+document.addEventListener("keydown", onKeyDown);
+document.addEventListener("keyup", onKeyUp);
+
+let prevTime = performance.now();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const time = performance.now();
+
+  // 前進後進判定
+  direction.z = Number(moveForward) - Number(moveBackward);
+  direction.x = Number(moveRight) - Number(moveLeft);
+
+  //ポインターがONになったら
+  if (controls.isLocked) {
+    const delta = (time - prevTime) / 1000;
+
+    // 減衰
+    velocity.z -= velocity.z * 5.0 * delta;
+    velocity.x -= velocity.x * 5.0 * delta;
+
+    if (moveForward || moveBackward) {
+      velocity.z -= direction.z;
+    }
+    if (moveRight || moveLeft) {
+      velocity.x -= direction.x;
+    }
+
+    controls.moveForward(-velocity.z * delta);
+    controls.moveRight(-velocity.x * delta);
+  }
+
+  prevTime = time;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+/**
+ * 画面リサイズ設定
+ **/
+window.addEventListener("resize", onWindowResize);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
